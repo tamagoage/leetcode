102e8e532baac95bed97707882b55f4d56f04707
tamagoage <116415023+tamagoage@users.noreply.github.com>
2024-03-08 18:39:07 +0900

    70min

---
+++ Explore/Easy/Array/5SingleNumber.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        map<int, int> numMap;
+        for (int num : nums)
+        {
+            numMap.at(num)++;
+        }
+
+        int singleNum;
+        for (auto &pair : numMap)
+        {
+            if (pair.second == 1)
+            {
+                singleNum = pair.first;
+            }
+        }
+
+        return singleNum;
+    }
+};
